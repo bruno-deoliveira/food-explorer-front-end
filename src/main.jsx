54cd034bf62203c5de663cbed6c8e763c041f2b5
Bruno <brunoalmeida0805@gmail.com>
2023-08-02 21:26:37 -0300

    add pages folders

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Dish } from './pages/Dish/index.jsx'
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Dish />
+  </React.StrictMode>
+);
---
+++ src/pages/Dish/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export function Dish () {
+  
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <span>Bruno Oliveira</span>
+    </div>
+  );
+}
