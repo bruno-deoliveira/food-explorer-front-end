1977921f6b25c621c8c1486bb8b184a7f94306ad
Bruno <brunoalmeida0805@gmail.com>
2023-09-01 21:05:28 -0300

    creation of hooks and services folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import GlobalStyles from "./styles/global"; 
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global";
+
+import { AuthProvider } from "./hooks/auth";
 
 import theme from "./styles/theme";
 
-import { Dish } from './pages/Dish/index.jsx'
-
-
+import { Routes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Dish />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
